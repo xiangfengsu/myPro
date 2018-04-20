a271b22016867864d8f7d5cefeebd4a29c710960
苏祥凤 <suxiangfeng@ebaochina.cn>
2018-04-20 14:04:26 +0800

    update

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
-import moment from 'moment';
-import config from '../config';
+import moment from "moment";
+import config from "../config";
 
 // logs('config', config);
 const { detailPagePathList } = config;
@@ -11,14 +11,14 @@ export function getTimeDistance(type) {
   const now = new Date();
   const oneDay = 1000 * 60 * 60 * 24;
 
-  if (type === 'today') {
+  if (type === "today") {
     now.setHours(0);
     now.setMinutes(0);
     now.setSeconds(0);
     return [moment(now), moment(now.getTime() + (oneDay - 1000))];
   }
 
-  if (type === 'week') {
+  if (type === "week") {
     let day = now.getDay();
     now.setHours(0);
     now.setMinutes(0);
@@ -30,33 +30,40 @@ export function getTimeDistance(type) {
       day -= 1;
     }
 
-    const beginTime = now.getTime() - (day * oneDay);
+    const beginTime = now.getTime() - day * oneDay;
 
-    return [moment(beginTime), moment(beginTime + ((7 * oneDay) - 1000))];
+    return [moment(beginTime), moment(beginTime + (7 * oneDay - 1000))];
   }
 
-  if (type === 'month') {
+  if (type === "month") {
     const year = now.getFullYear();
     const month = now.getMonth();
-    const nextDate = moment(now).add(1, 'months');
+    const nextDate = moment(now).add(1, "months");
     const nextYear = nextDate.year();
     const nextMonth = nextDate.month();
 
-    return [moment(`${year}-${fixedZero(month + 1)}-01 00:00:00`), moment(moment(`${nextYear}-${fixedZero(nextMonth + 1)}-01 00:00:00`).valueOf() - 1000)];
+    return [
+      moment(`${year}-${fixedZero(month + 1)}-01 00:00:00`),
+      moment(
+        moment(
+          `${nextYear}-${fixedZero(nextMonth + 1)}-01 00:00:00`
+        ).valueOf() - 1000
+      )
+    ];
   }
 
-  if (type === 'year') {
+  if (type === "year") {
     const year = now.getFullYear();
 
     return [moment(`${year}-01-01 00:00:00`), moment(`${year}-12-31 23:59:59`)];
   }
 }
 
-export function getPlainNode(nodeList, parentPath = '') {
+export function getPlainNode(nodeList, parentPath = "") {
   const arr = [];
-  nodeList.forEach((node) => {
+  nodeList.forEach(node => {
     const item = node;
-    item.path = `${parentPath}/${item.path || ''}`.replace(/\/+/g, '/');
+    item.path = `${parentPath}/${item.path || ""}`.replace(/\/+/g, "/");
     item.exact = true;
     if (item.children && !item.component) {
       arr.push(...getPlainNode(item.children, item.path));
@@ -71,37 +78,40 @@ export function getPlainNode(nodeList, parentPath = '') {
 }
 
 export function digitUppercase(n) {
-  const fraction = ['角', '分'];
-  const digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
-  const unit = [
-    ['元', '万', '亿'],
-    ['', '拾', '佰', '仟'],
-  ];
+  const fraction = ["角", "分"];
+  const digit = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"];
+  const unit = [["元", "万", "亿"], ["", "拾", "佰", "仟"]];
   let num = Math.abs(n);
-  let s = '';
+  let s = "";
   fraction.forEach((item, index) => {
-    s += (digit[Math.floor(num * 10 * (10 ** index)) % 10] + item).replace(/零./, '');
+    s += (digit[Math.floor(num * 10 * 10 ** index) % 10] + item).replace(
+      /零./,
+      ""
+    );
   });
-  s = s || '整';
+  s = s || "整";
   num = Math.floor(num);
   for (let i = 0; i < unit[0].length && num > 0; i += 1) {
-    let p = '';
+    let p = "";
     for (let j = 0; j < unit[1].length && num > 0; j += 1) {
       p = digit[num % 10] + unit[1][j] + p;
       num = Math.floor(num / 10);
     }
-    s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+    s = p.replace(/(零.)*零$/, "").replace(/^$/, "零") + unit[0][i] + s;
   }
 
-  return s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
+  return s
+    .replace(/(零.)*零元/, "元")
+    .replace(/(零.)+/g, "零")
+    .replace(/^整$/, "零元整");
 }
 
 function getRelation(str1, str2) {
   if (str1 === str2) {
-    console.warn('Two path are equal!');  // eslint-disable-line
+    console.warn("Two path are equal!"); // eslint-disable-line
   }
-  const arr1 = str1.split('/');
-  const arr2 = str2.split('/');
+  const arr1 = str1.split("/");
+  const arr2 = str2.split("/");
   if (arr2.every((item, index) => item === arr1[index])) {
     return 1;
   } else if (arr1.every((item, index) => item === arr2[index])) {
@@ -111,9 +121,10 @@ function getRelation(str1, str2) {
 }
 
 export function getRoutes(path, routerData) {
-  let routes = Object.keys(routerData).filter(routePath =>
-    routePath.indexOf(path) === 0 && routePath !== path);
-  routes = routes.map(item => item.replace(path, ''));
+  let routes = Object.keys(routerData).filter(
+    routePath => routePath.indexOf(path) === 0 && routePath !== path
+  );
+  routes = routes.map(item => item.replace(path, ""));
   let renderArr = [];
   renderArr.push(routes[0]);
   for (let i = 1; i < routes.length; i += 1) {
@@ -124,20 +135,22 @@ export function getRoutes(path, routerData) {
       renderArr.push(routes[i]);
     }
   }
-  const renderRoutes = renderArr.map((item) => {
-    const exact = !routes.some(route => route !== item && getRelation(route, item) === 1);
+  const renderRoutes = renderArr.map(item => {
+    const exact = !routes.some(
+      route => route !== item && getRelation(route, item) === 1
+    );
     return {
       ...routerData[`${path}${item}`],
       key: `${path}${item}`,
       path: `${path}${item}`,
-      exact,
+      exact
     };
   });
   return renderRoutes;
 }
-export function formatter(data = [], parentPath = '', parentAuthority) {
+export function formatter(data = [], parentPath = "", parentAuthority) {
   // logs('data', data);
-  return data.map((item) => {
+  return data.map(item => {
     let { path } = item;
     if (!isUrl(path)) {
       path = parentPath + item.path;
@@ -145,32 +158,42 @@ export function formatter(data = [], parentPath = '', parentAuthority) {
     const result = {
       ...item,
       path,
-      authority: item.authority || parentAuthority,
+      authority: item.authority || parentAuthority
     };
     if (item.children) {
-      result.children = formatter(item.children, `${parentPath}${item.path}/`, item.authority);
+      result.children = formatter(
+        item.children,
+        `${parentPath}${item.path}/`,
+        item.authority
+      );
     }
     return result;
   });
 }
 export function menuDataPathFormater(menuData) {
   const list = [];
-  const exceptionPath = ['/exception', '/exception/403', '/exception/404', '/exception/500'];
+  const exceptionPath = [
+    "/exception",
+    "/exception/403",
+    "/exception/404",
+    "/exception/500"
+  ];
+  console.log("menuDatassss", menuData);
   (function dataFormater(menuData) {
     menuData.forEach(item => {
-      if (item.children) {
-        list.push(`/${item.path}`)
-        dataFormater(item.children)
+      if (item.children && item.children.length > 0) {
+        list.push(`/${item.path}`);
+        dataFormater(item.children);
       } else {
         // 判断menu path页面是否跳转到新页面，添加新页面访问权限
         const detailPath = detailPagePathList.find(info => {
-          return info.key === `/${item.path}`
+          return info.key === `/${item.path}`;
         });
         detailPath && list.push(...detailPath.detailPathChild);
-        list.push(`/${item.path}`)
+        list.push(`/${item.path}`);
       }
-    })
-  })(menuData)
+    });
+  })(menuData);
   return [...list, ...exceptionPath];
 }
 export function menuAuthority(menuDatas, path) {
@@ -178,47 +201,52 @@ export function menuAuthority(menuDatas, path) {
   let isAuthority = true;
   // logs('len', menuDatas.length);
   if (menuDatas.length > 4) {
-    isAuthority = menuDatas.indexOf(path) !== -1 ? true : false
+    isAuthority = menuDatas.indexOf(path) !== -1 ? true : false;
   }
   return isAuthority;
-
 }
 export function formaterObjectValue(obj) {
   const newObj = {};
-  if (!obj || Object.prototype.toString.call(obj) !== '[object Object]') {
-    return newObj
+  if (!obj || Object.prototype.toString.call(obj) !== "[object Object]") {
+    return newObj;
   }
   for (let key in obj) {
     if (obj.hasOwnProperty(key)) {
-      newObj[key] = obj[key] === undefined ? '' : obj[key];
+      newObj[key] = obj[key] === undefined ? "" : obj[key];
     }
   }
   return newObj;
 }
 export function formItemAddInitValue(formItems, currentItem) {
-  if (!currentItem || Object.prototype.toString.call(currentItem) !== '[object Object]') {
-    return formItems
+  if (
+    !currentItem ||
+    Object.prototype.toString.call(currentItem) !== "[object Object]"
+  ) {
+    return formItems;
   }
   const newFormItems = [];
   const currItemKeys = Object.keys(currentItem);
   if (currItemKeys.length > 0) {
     formItems.forEach(item => {
-      const index = currItemKeys.indexOf(item['key']);
+      const index = currItemKeys.indexOf(item["key"]);
       if (index > -1) {
-        newFormItems.push(Object.assign(item, { initialValue: currentItem[currItemKeys[index]] }));
+        newFormItems.push(
+          Object.assign(item, {
+            initialValue: currentItem[currItemKeys[index]]
+          })
+        );
       } else {
         newFormItems.push(item);
       }
-    })
+    });
   } else {
     formItems.forEach(item => {
       newFormItems.push(Object.assign(item, { initialValue: undefined }));
-    })
+    });
   }
 
   return newFormItems;
 }
-
 
 /* eslint no-useless-escape:0 */
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g;
